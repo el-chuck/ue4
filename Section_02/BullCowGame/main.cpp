d71bad0a0871cb8abef3f1bae2aa5978ec00b8aa
Bernhard Pickenbrock <be.pickenbrock@gmail.com>
2018-06-03 09:12:24 +0200

    S2L22 use getline for input

--- Section_02/BullCowGame/main.cpp
+++ Section_02/BullCowGame/main.cpp
@@ -10,12 +10,19 @@ int main()
 	// introduction
 	cout << "Welcome to Bulls and Cows, a fun word game.\n";
 	cout << "Can you guess the " << WORD_LENGTH;
-	cout << " letter isogram i'm thinking of?\n";
+	cout << " letter isogram i'm thinking of?\n\n";
 	
 	// read player guess
 	string Guess = "";
 	cout << "What is your guess?";
-	cin >> Guess;
+	getline(cin, Guess);
+
+	// repeat back player guess
+	cout << "Your guess is: " << Guess << endl;
+
+	// read player guess
+	cout << "What is your guess? ";
+	getline(cin, Guess);
 
 	// repeat back player guess
 	cout << "Your guess is: " << Guess << endl;
