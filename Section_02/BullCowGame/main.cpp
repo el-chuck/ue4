942023d7893e8c28d987f0d5849924241e3fd3da
Bernhard Pickenbrock <be.pickenbrock@gmail.com>
2018-06-03 09:02:00 +0200

    S2L21 player input

--- Section_02/BullCowGame/main.cpp
+++ Section_02/BullCowGame/main.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <string>
 
 using namespace std;
 
@@ -6,7 +7,18 @@ int main()
 {
 	constexpr int WORD_LENGTH = 5;
 
-	cout << "Welcome to Bulls and Cows, a fun word game." << endl;
-	cout << "Can you guess the " << WORD_LENGTH << " letter isogram i'm thinking of?" << endl;
+	// introduction
+	cout << "Welcome to Bulls and Cows, a fun word game.\n";
+	cout << "Can you guess the " << WORD_LENGTH;
+	cout << " letter isogram i'm thinking of?\n";
+	
+	// read player guess
+	string Guess = "";
+	cout << "What is your guess?";
+	cin >> Guess;
+
+	// repeat back player guess
+	cout << "Your guess is: " << Guess << endl;
+	
 	return 0;
 }
